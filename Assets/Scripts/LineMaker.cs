667c9e045e75ca874a1a1a66c33f4b26aa53220e
hmuaz <muazsenol1@gmail.com>
2024-09-07 17:40:11 +0300

    linemake

--- Assets/Scripts/LineMaker.cs
+++ Assets/Scripts/LineMaker.cs
@@ -8,8 +8,15 @@ public class LineMaker : MonoBehaviour
 
     public void ClickedGrip()
     {
+        // LineRenderer'ın pozisyon sayısını en az 2 olacak şekilde ayarla
+        /* if (lastNode.lineRenderer.positionCount < 2)
+        {
+            lastNode.lineRenderer.positionCount = 2; // En az iki pozisyon olmalı
+        } */
+
+        // Pozisyonları ayarla
         lastNode.lineRenderer.SetPosition(1, transform.position);
+        lastNode.lineRenderer.startWidth = 0.01f;
+        lastNode.lineRenderer.endWidth = 0.01f;
     }
-
-
-} 
+}
